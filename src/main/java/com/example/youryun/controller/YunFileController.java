4c849a1d748e486a4b53923f4aaf5cef031c8a3c
RiceRollsMan <3533734346@qq.com>
2022-05-17 14:15:56 +0800

    把那个删除state改成只修改自己的state。

--- src/main/java/com/example/youryun/controller/YunFileController.java
+++ src/main/java/com/example/youryun/controller/YunFileController.java
@@ -142,57 +142,57 @@ public class YunFileController {
     * 2022/5/15
     * 功能:通过传入文件id下载文件
     * */
-    /*后台功能版
-    @GetMapping("/toDownLoadFile")
-    public void toDownLoadFile(@RequestParam("id") int id,
-                               HttpServletResponse response){
-        try {
-            //通过文件的id获取到指定下载文件的对象
-            YunFile yunFile= yunFileService.downloadFile(id);
-            System.out.println(yunFile);
-            // path是指想要下载的文件的路径
-            File file = new File(yunFile.getFile_path());
-            // 获取文件名
-            String filename = yunFile.getFile_name();
-            // 获取文件后缀名
-            //String ext = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+    //后台功能版
+//    @GetMapping("/toDownLoadFile")
+//    public void toDownLoadFile(@RequestParam("id") int id,
+//                               HttpServletResponse response){
+//        try {
+//            //通过文件的id获取到指定下载文件的对象
+//            YunFile yunFile= yunFileService.downloadFile(id);
+//            System.out.println(yunFile);
+//            // path是指想要下载的文件的路径
+//            File file = new File(yunFile.getFile_path());
+//            // 获取文件名
+//            String filename = yunFile.getFile_name();
+//            // 获取文件后缀名
+//            //String ext = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+//
+//            // 将文件写入输入流
+//            FileInputStream fileInputStream = new FileInputStream(file);
+//            InputStream fis = new BufferedInputStream(fileInputStream);
+//            byte[] buffer = new byte[fis.available()];
+//            fis.read(buffer);
+//            fis.close();
+//
+//            // 清空response
+//            response.reset();
+//            // 设置response的Header
+//            response.setCharacterEncoding("UTF-8");
+//            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
+//            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
+//            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
+//
+//            //没区别啊
+//            //response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
+//            response.addHeader("Content-Disposition", "inline;filename=" + URLEncoder.encode(filename, "UTF-8"));
+//            // 告知浏览器文件的大小
+//            response.addHeader("Content-Length", "" + file.length());
+//            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+//            response.setContentType("application/octet-stream");
+//            outputStream.write(buffer);
+//            outputStream.flush();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
 
-            // 将文件写入输入流
-            FileInputStream fileInputStream = new FileInputStream(file);
-            InputStream fis = new BufferedInputStream(fileInputStream);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            fis.close();
-
-            // 清空response
-            response.reset();
-            // 设置response的Header
-            response.setCharacterEncoding("UTF-8");
-            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
-            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
-            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
-
-            //没区别啊
-            //response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
-            response.addHeader("Content-Disposition", "inline;filename=" + URLEncoder.encode(filename, "UTF-8"));
-            // 告知浏览器文件的大小
-            response.addHeader("Content-Length", "" + file.length());
-            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-            response.setContentType("application/octet-stream");
-            outputStream.write(buffer);
-            outputStream.flush();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-    */
 
     /*整合VUE版*/
     @ResponseBody
     @GetMapping("/toDownLoadFile")
     public void toDownLoadFile(@RequestBody @RequestParam("id") int id,
                                HttpServletResponse response){
-        System.out.println(response);
+        System.out.println(id);
         try {
             //通过文件的id获取到指定下载文件的对象
             System.out.println("1");
@@ -215,32 +215,31 @@ public class YunFileController {
             System.out.println("2");
 
             // 清空response
-//            response.reset();
-//            // 设置response的Header
-//            response.setCharacterEncoding("UTF-8");
-//            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
-//            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
-//            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
-//
-//            //没区别啊
-//            //response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
-//            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
-//            // 告知浏览器文件的大小
-//            response.addHeader("Content-Length", "" + file.length());
-//            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
-//            System.out.println(response);
-//           // HttpServletResponse res=response;
-//            response.setContentType("application/octet-stream");
-//            outputStream.write(buffer);
-//            outputStream.flush();
-//            System.out.println("3");
-//            //return res;
+            response.reset();
+            // 设置response的Header
+            response.setCharacterEncoding("UTF-8");
+            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
+            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
+            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
+
+            //没区别啊
+            //response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
+            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
+            // 告知浏览器文件的大小
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+            System.out.println(response);
+           // HttpServletResponse res=response;
+            response.setContentType("application/octet-stream");
+            outputStream.write(buffer);
+            outputStream.flush();
+            System.out.println("3");
+            //return res;
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         System.out.println(4);
         //return null;
-
     }
 
     /*
